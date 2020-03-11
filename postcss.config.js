2d3e1e77bfd842737f86170df927e0484c96f5b1
Wil Pirino <wpirino1@gmail.com>
2020-03-11 17:26:57 -0600

    rdy for deploy

--- postcss.config.js
+++ postcss.config.js
@@ -8,7 +8,7 @@ module.exports = {
       ? [
           purgecss({
             content: ["./src/**/*.js", "./public/index.html"],
-            defaultExtractor: content => content.match(/[A-Za-z0-9-_:]+/g) || []
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_://]+/g) || []
           }),
           require("cssnano")({
             preset: "default"
